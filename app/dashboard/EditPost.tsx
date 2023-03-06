adfe6825aa908f7f269022f805acc1195a921c28
Brandon Garcia <grandonleebarcia@gmail.com>
2023-03-06 14:30:17 -0800

    add toggle and mutation call to delete post

--- app/dashboard/EditPost.tsx
+++ app/dashboard/EditPost.tsx
@@ -1,7 +1,10 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import { useState } from 'react';
+import { useMutation } from "@tanstack/react-query";
+import Image from "next/image";
+import { useState } from "react";
+import Toggle from "./Toggle";
+import axios from "axios";
 
 type EditProps = {
   id: string;
@@ -22,21 +25,32 @@ export default function EditPost({
   comments,
   id,
 }: EditProps) {
+  const [toggle, setToggle] = useState(false);
+
+  //Delete Post
+  const { mutate } = useMutation(
+    async (id: string) =>
+      await axios.delete("/api/posts/deletePost", { data: id })
+  );
+
   return (
-    <div className="bg-white my-8 p-8 rounded-lg">
-      <div className="flex items-center gap-2">
-        <Image width={32} height={32} src={avatar} alt="avatar"></Image>
-        <h3 className="font-bold text-gray-700">{name}</h3>
+    <>
+      <div className="bg-white my-8 p-8 rounded-lg">
+        <div className="flex items-center gap-2">
+          <Image width={32} height={32} src={avatar} alt="avatar"></Image>
+          <h3 className="font-bold text-gray-700">{name}</h3>
+        </div>
+        <div className="my-8">
+          <p className="break-all">{title}</p>
+        </div>
+        <div className="flex items-center gap-4">
+          <p className="text-sm font-bold text-gray-700">
+            {comments?.length} Comments
+          </p>
+          <button className="text-sm font-bold text-red-500">Delete</button>
+        </div>
       </div>
-      <div className="my-8">
-        <p className="break-all">{title}</p>
-      </div>
-      <div className="flex items-center gap-4">
-        <p className="text-sm font-bold text-gray-700">
-          {comments?.length} Comments
-        </p>
-        <button className="text-sm font-bold text-red-500">Delete</button>
-      </div>
-    </div>
+      {toggle && <Toggle />}
+    </>
   );
 }
