12599e4f84fe377ac00a4dac34263151e0490035
Brandon Garcia <grandonleebarcia@gmail.com>
2023-03-07 17:46:22 -0800

    changed req method to POST

--- pages/api/posts/addComment.ts
+++ pages/api/posts/addComment.ts
@@ -8,34 +8,35 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) {
+    return res.status(401).json({ message: 'Please sign in' });
+  }
+
+  //Get User
+
+  const prismaUser = await prisma.user.findUnique({
+    where: { email: session?.user?.email },
+  });
+
+  const { title, postId } = req.body.data;
+  if (!title.length) {
+    return res.status(401).json({ message: 'Please enter something' });
+  }
+
   if (req.method === 'POST') {
-    const session = await getServerSession(req, res, authOptions);
-    if (!session) {
-      return res.status(401).json({ message: 'Please sign in' });
-    }
-
-    //Get User
-
-    const prismaUser = await prisma.user.findUnique({
-      where: { email: session?.user?.email },
-    });
-
     //Add a Comment
-
+    console.log(req.method);
+    console.log(prismaUser, 'prisma User');
     try {
-      const { title, postId } = req.body.data;
-      if (!title.length) {
-        return res.status(401).json({ message: 'Please enter something' });
-      }
-
-      const result = prisma.comment.create({
+      const result = await prisma.comment.create({
         data: {
           message: title,
           userId: prismaUser?.id,
-          postId,
+          postId: postId,
         },
       });
-      console.log(JSON.stringify(result));
+      console.log(JSON.stringify(result), 'result');
       res.status(200).json(result);
     } catch (err) {
       res.status(403).json({ err: 'Error has occured while making a post' });
